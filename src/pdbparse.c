22ed6a5e348dbf10a70177ef1d2ba98fb4f1a86a
shareef12 <shareef12@twelvetacos.com>
2020-04-01 14:28:12 +0000

    pdbparse: print section characteristics for --sections

--- src/pdbparse.c
+++ src/pdbparse.c
@@ -119,14 +119,28 @@ void print_sections(void *pdb)
 
     for (uint32_t i = 0; i < nr_sections; i++) {
         const struct image_section_header *s = &sections[i];
-        printf("  [%2u] %-8.*s  0x%06x 0x%016x 0x%06x 0x%06x flag\n",
+        printf("  [%2u] %-8.*s  0x%06x 0x%016x 0x%06x 0x%06x 0x%08x\n",
             i,
             IMAGE_SIZEOF_SHORT_NAME, s->name,
             s->pointer_to_raw_data,
             s->virtual_address,
             s->size_of_raw_data,
-            s->misc.virtual_size);
+            s->misc.virtual_size,
+            s->characteristics);
     }
+
+    puts("");
+    puts("Flags:");
+    puts("  IMAGE_SCN_CNT_CODE                  0x00000020");
+    puts("  IMAGE_SCN_CNT_INITIALIZED_DATA      0x00000040");
+    puts("  IMAGE_SCN_CNT_UNINITIALIZED_DATA    0x00000080");
+    puts("  IMAGE_SCN_MEM_DISCARDABLE           0x02000000");
+    puts("  IMAGE_SCN_MEM_NOT_CACHED            0x04000000");
+    puts("  IMAGE_SCN_MEM_NOT_PAGED             0x08000000");
+    puts("  IMAGE_SCN_MEM_SHARED                0x10000000");
+    puts("  IMAGE_SCN_MEM_EXECUTE               0x20000000");
+    puts("  IMAGE_SCN_MEM_READ                  0x40000000");
+    puts("  IMAGE_SCN_MEM_WRITE                 0x80000000");
 }
 
 
